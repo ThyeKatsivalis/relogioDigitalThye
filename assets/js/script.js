8178471cf80c3eb6ddd05086dec4afe859828444
ThyeKatsivalis <titomalb@gmail.com>
2023-08-07 02:25:02 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+  let dataDeHoje = new Date();
+  let hr = dataDeHoje.getHours();
+  let min = dataDeHoje.getMinutes();
+  let sec = dataDeHoje.getSeconds();
+
+  if(hr < 10) hr = '0' + hr
+
+  if(min < 10) min = '0' + min
+
+  if(sec < 10) sec = '0' + sec
+  
+  horas.textContent = hr
+  minutos.textContent = min
+  segundos.textContent = sec
+
+})
